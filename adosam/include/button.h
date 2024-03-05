6a479e555ae2126f3aa4e896592ebab6ccbdd5d8
MariusAurelius <marius.henriksen@etu.univ-lyon1.fr>
2024-03-05 11:52:34 +0100

    start doxygen doc

--- adosam/include/button.h
+++ adosam/include/button.h
@@ -5,11 +5,29 @@
 
 #include "image.h"
 
-
+/**
+ * @class Button
+ * 
+ * @brief Classe représentant une image cliquable.
+ * 
+ * Cette classe hérite de la classe Image. Elle permet de manipuler un bouton.
+ */
 class Button : public Image{
 public:
-
-    Button(SDL_Renderer* source_renderer, const std::string& file_path, const float& x_pos, const float& y_pos, const int& width, const int& height);
+    /**
+     * @brief Constructeur de la classe Button.
+     * 
+     * Initialise une image avec les champs qui sont en paramètre.
+     * 
+     * @param source_renderer 
+     * @param file_path 
+     * @param x_pos
+     * @param y_pos
+     * @param width
+     * @param height
+     */
+    Button(SDL_Renderer* source_renderer, const std::string& file_path, 
+           const float& x_pos, const float& y_pos, const int& width, const int& height);
     
     ~Button();
 
